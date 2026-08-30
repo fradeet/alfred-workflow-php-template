f7c565e55fced29d68c4f023e29920e5afdfa1ce
Fradeet Y. <41571697+fradeet@users.noreply.github.com>
2026-08-30 15:10:58 +0800

    feat(adapter): demonstrate reading Alfred environment variable

--- workflow/src/AlfredAdapters/Hello.php
+++ workflow/src/AlfredAdapters/Hello.php
@@ -7,6 +7,7 @@ use Workflow\AlfredAdapters\Type\AlfredSF;
 use Workflow\AlfredAdapters\Type\AlfredSFItem;
 use Workflow\Hello;
 
+use function Workflow\AlfredAdapters\Support\alfredEnvironmentVariable;
 use function Workflow\AlfredAdapters\Support\run;
 
 require dirname(__DIR__, 2).'/vendor/autoload.php';
@@ -14,7 +15,10 @@ require dirname(__DIR__, 2).'/vendor/autoload.php';
 run(
     fn (array $arguments): AlfredSF => new AlfredSF(
         items: [
-            new AlfredSFItem(title: (new Hello())(...$arguments)),
+            new AlfredSFItem(
+                title: (new Hello())(...$arguments),
+                subtitle: alfredEnvironmentVariable('alfred_workflow_name'),
+            ),
         ],
     ),
 );
